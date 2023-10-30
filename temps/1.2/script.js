bf0f943d1a8e1f2712f36a0bcfa86cc044d7135c
Leon Berkemeier <leonberkemeier@gmail.com>
2023-10-30 15:52:30 +0100

    first

---
+++ temps/1.2/script.js
@@ -0,0 +1,8 @@
+// ===== Multitext ======
+var typingEffect = new Typed (".multiText",{
+    strings:["Coder","Leon","Showman","entertainer"],
+    loop: true,
+    typeSpeed: 200,
+    backSpeed: 300,
+    backDelay: 1500,
+});
