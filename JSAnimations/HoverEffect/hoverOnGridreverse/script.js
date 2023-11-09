0918158784427d4650ccc2b19676049bbd6d6dcf
Leon Berkemeier <leonberkemeier@gmail.com>
2023-11-09 11:40:51 +0100

    SkillsNCourses

--- JSAnimations/HoverEffect/hoverOnGridreverse/script.js
+++ JSAnimations/HoverEffect/hoverOnGridreverse/script.js
@@ -144,33 +144,3 @@ var coll = document.getElementsByClassName("collapsible");
       
     };
 
-
-// hover effect 
-
-const handleOnMouseMove = e => {
-  const {currentTarget: target} = e;
-
-  const rect = target.getBoundingClientRect(),
-      x = e.clientX - rect.left,
-      y = e.clientY - rect.top;
-
-  target.style.setProperty("--mouse-x", `${x}px`);
-  target.style.setProperty("--mouse-y", `${y}px`);
-};
-
-for(const card of document.querySelectorAll(".card")){
-  card.onmousemove = e => handleOnMouseMove(e);
-};
-
-document.getElementById("grid_skills").onmousemove = e => {
-  for(const card of document.getElementsByClassName("skillsItem")){
-  const rect = card.getBoundingClientRect(),
-      x = e.clientX - rect.left,
-      y = e.clientY - rect.top;
-
-  card.style.setProperty("--mouse-x", `${x}px`);
-  card.style.setProperty("--mouse-y", `${y}px`);
-
-  }
-};
-
