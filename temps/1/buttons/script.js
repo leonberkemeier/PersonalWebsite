e2c988faa4498628dcf877ac7023134fa2dfd9eb
Leon Berkemeier <leonberkemeier@gmail.com>
2023-11-09 12:52:39 +0100

    buttonsNbuttonsanimation

---
+++ temps/1/buttons/script.js
@@ -0,0 +1,23 @@
+
+
+document.getElementById('btn-vsco').addEventListener("mouseover", function() {
+    let icon = this.querySelector('.button-icon');
+    icon.classList.toggle('rotate');
+});
+
+document.getElementById('btn-insta').addEventListener("mouseover", function() {
+    let icon = this.querySelector('.button-icon');
+    icon.classList.toggle('rotate');
+});
+
+document.getElementById('btn-li').addEventListener("mouseover", function() {
+    let icon = this.querySelector('.button-icon');
+    icon.classList.toggle('rotate');
+});
+
+document.getElementById('btn-gh').addEventListener("mouseover", function() {
+    let icon = this.querySelector('.button-icon');
+    icon.classList.toggle('rotate');
+});
+
+//alert('this sucks');
---
+++ temps/1/navbar/script.js
@@ -0,0 +1,31 @@
+
+const letters ="ABCDEFGHIJKLMNOPQSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+let interval = null;
+
+document.querySelector(".nav__logo h1").onmouseover = event =>{
+    let iteration = 0;
+
+    clearInterval(interval);
+
+
+    interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter,index) => {
+            if(index < iteration) {
+                return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 26)]
+        })
+        .join("");
+
+    if(iteration >= event.target.dataset.value.length){
+        clearInterval(interval);
+    }
+
+    iteration += 1/3;
+    }, 80);
+    
+};
+
